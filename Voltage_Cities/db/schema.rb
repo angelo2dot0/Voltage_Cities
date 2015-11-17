b98aac06cbf66d1bc0219a1edc7ff232872388db
Racha <br_bia@esi.dz>
2015-11-16 18:52:50 -0800

    adding comment model

--- Voltage_Cities/db/schema.rb
+++ Voltage_Cities/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151116191122) do
+ActiveRecord::Schema.define(version: 20151117021422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20151116191122) do
     t.string   "name"
     t.string   "country"
     t.string   "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.integer  "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
