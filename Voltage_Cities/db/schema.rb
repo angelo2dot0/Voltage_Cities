182d12ffffcb3e73093b7a516bbf261606de8b77
margauxwithanx <margaux@margauxwithanx.com>
2015-11-16 18:46:04 -0800

    adding styling

--- Voltage_Cities/db/schema.rb
+++ Voltage_Cities/db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20151116191122) do
-
+ActiveRecord::Schema.define(version: 20151116203523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
